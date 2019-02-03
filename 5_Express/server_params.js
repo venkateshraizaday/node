8d06a5520c92d994fe7156ec34ea6b2e883211a5
Venkatesh Raizaday <venkatesh.raizaday@gmail.com>
2019-02-02 20:56:47 -0500

    Understanding query params.

--- 5_Express/server_params.js
+++ 5_Express/server_params.js
@@ -13,5 +13,16 @@ app.get("/data/:id", (req,res) => {
 	`)
 })
 
+// /api/data?user=venky&id=22
+app.get("/api/data", (req, res)=> {
+	let user = req.query.user;
+	let id = req.query.id;
+	
+	res.send({
+		user,
+		id
+	})
+})
+
 const port = process.env.PORT || 3000
 app.listen(port)
