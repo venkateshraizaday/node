faf1967684646ad23b09c1913e3d814c0a64e434
Venkatesh Raizaday <venkatesh.raizaday@gmail.com>
2019-02-02 20:41:13 -0500

    Improving base express server...

--- 5_Express/server.js
+++ 5_Express/server.js
@@ -1,4 +1,22 @@
 const express = require('express')
 
-const app = express();
-app.listen(3000)
+const app = express();
+app.get("/", (req,res)=>{
+	res.send(`
+	<html>
+		<body>
+			<h1>Holla from express!!!</h1>
+		</body>
+	</html>
+	`)
+})
+
+app.get("/data", (req,res)=>{
+	res.send({
+		name:"Francis",
+		lastname:"Ho"
+	})
+})
+
+const port = process.env.PORT || 3000
+app.listen(port)
